e73dc18952fe8ee35939b9e2247f8f0bcd07b7fc
Dhipa Poltak <dhipa2poltak@gmail.com>
2023-10-17 01:17:01 +0700

    refactoring BaseFragment

--- framework/src/main/java/com/dpfht/android/democityweather/framework/commons/base/BaseFragment.kt
+++ framework/src/main/java/com/dpfht/android/democityweather/framework/commons/base/BaseFragment.kt
@@ -1,6 +1,5 @@
 package com.dpfht.android.democityweather.framework.commons.base
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,10 +8,7 @@ import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
-import com.dpfht.android.democityweather.framework.commons.base.di.DaggerBaseComponent
-import com.dpfht.android.democityweather.framework.di.dependency.NavigationServiceDependency
 import com.dpfht.android.democityweather.framework.navigation.NavigationService
-import dagger.hilt.android.EntryPointAccessors
 import javax.inject.Inject
 
 open class BaseFragment<VDB: ViewDataBinding>(
@@ -23,16 +19,6 @@ open class BaseFragment<VDB: ViewDataBinding>(
 
   @Inject
   protected lateinit var navigationService: NavigationService
-
-  override fun onAttach(context: Context) {
-    super.onAttach(context)
-
-    DaggerBaseComponent.builder()
-      .context(requireContext())
-      .navDependency(EntryPointAccessors.fromActivity(requireActivity(), NavigationServiceDependency::class.java))
-      .build()
-      .inject(this as BaseFragment<ViewDataBinding>)
-  }
 
   override fun onCreateView(
     inflater: LayoutInflater,
