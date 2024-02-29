5e6f7cc099d937d3092bd248b017a2811da28c71
Dhipa Poltak <dhipa2poltak@gmail.com>
2024-03-01 04:21:08 +0700

    increasing targetsdkVersion to 34

--- buildSrc/src/main/java/ConfigData.kt
+++ buildSrc/src/main/java/ConfigData.kt
@@ -1,7 +1,7 @@
 object ConfigData {
-  const val compileSdkVersion = 33
+  const val compileSdkVersion = 34
   const val minSdkVersion = 21
-  const val targetSdkVersion = 33
+  const val targetSdkVersion = 34
   const val versionCode = 6
   const val versionName = "1.5"
   const val versionCodeDev = 6
