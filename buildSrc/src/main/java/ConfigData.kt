3e60454bf5e41eb6d2ab08cfe36d17bb6e89f639
Dhipa Poltak <dhipa2poltak@gmail.com>
2024-03-01 07:07:13 +0700

    managing dependencies

--- buildSrc/src/main/java/ConfigData.kt
+++ buildSrc/src/main/java/ConfigData.kt
@@ -1,3 +1,5 @@
+import org.gradle.api.JavaVersion
+
 object ConfigData {
   const val compileSdkVersion = 34
   const val minSdkVersion = 21
@@ -6,4 +8,7 @@ object ConfigData {
   const val versionName = "1.5"
   const val versionCodeDev = 6
   const val versionNameDev = "1.5"
+  val sourceCompatibilityVersion = JavaVersion.VERSION_17
+  val targetCompatibilityVersion = JavaVersion.VERSION_17
+  const val jvmTargetVersion = "17"
 }
