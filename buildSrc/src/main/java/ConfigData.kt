9b4f861aff85f9c627435f68a627c8995ba07e2b
Dhipa Poltak <dhipa2poltak@gmail.com>
2025-09-12 15:32:00 +0700

    increasing app version to 7 (1.6) and releasing to GitHub.

--- buildSrc/src/main/java/ConfigData.kt
+++ buildSrc/src/main/java/ConfigData.kt
@@ -4,10 +4,10 @@ object ConfigData {
   const val compileSdkVersion = 34
   const val minSdkVersion = 21
   const val targetSdkVersion = 34
-  const val versionCode = 6
-  const val versionName = "1.5"
-  const val versionCodeDev = 6
-  const val versionNameDev = "1.5"
+  const val versionCode = 7
+  const val versionName = "1.6"
+  const val versionCodeDev = 7
+  const val versionNameDev = "1.6"
   val sourceCompatibilityVersion = JavaVersion.VERSION_17
   val targetCompatibilityVersion = JavaVersion.VERSION_17
   const val jvmTargetVersion = "17"
